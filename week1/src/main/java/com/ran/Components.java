0e695f6814a9cd996017ad8c338b6662a297ac3a
chaeyoung <happy7yong83@gmail.com>
2025-09-14 20:27:33 +0900

    feat : 가장 기초 템플릿 컴포넌트 생성

---
+++ week1/src/main/java/com/ran/Components.java
@@ -0,0 +1,36 @@
+package com.ran;
+
+import java.util.ArrayList;
+
+public class Components {
+    public static void numberShow(String[] items) {
+            for(int Number=0;Number<items.length;Number++){
+                System.out.println(Number+1 +". "+ items[Number]);
+            }
+        }
+
+    public static void numberlessShow(String[] items){
+            for (String item : items) {
+                System.out.println(item);
+            }
+        }
+
+        public static void backShow(String[] items){
+            if(items != Main.ingredient.Action){
+                System.out.println("-> 뒤로가기");
+            }
+        }
+
+        public static void currentCookIngreShow(ArrayList<String> items){
+            System.out.print("현재 선택한 재료 :");
+            if(!items.isEmpty()){
+                for(String item : items){
+                    System.out.print(item+" ");
+                }
+            }else{
+                System.out.println("없음");
+            }
+        }
+
+}
+
