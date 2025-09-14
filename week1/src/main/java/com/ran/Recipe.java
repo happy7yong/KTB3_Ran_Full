7c0729c2156f67c044a08a7fd8874a8c7e049b7b
chaeyoung <happy7yong83@gmail.com>
2025-09-14 22:30:42 +0900

    feat : 2차 상속 - 카테고리 추가

--- week1/src/main/java/com/ran/Recipe.java
+++ week1/src/main/java/com/ran/Recipe.java
@@ -13,7 +13,7 @@ public class Recipe {
     }
 
     public boolean containsIngredient(Set<String> ingredients) {
-        return ingredients.equals(requireIngredients);//레시피에 있는 요소들이 모두 포함되어있는지확인
+        return ingredients.equals(requireIngredients);//레시피에 있는 요소들이 모두 포함되어있는지 확인
     }
 
     public void cook(){
@@ -21,45 +21,116 @@ public class Recipe {
     }
 }
 
+//카테고리 별 효과음
+// 달걀 요리 레시피 : 카테고리
+class EggRecipe extends Recipe {
+    EggRecipe(String name, Set<String> requireIngredients) {
+        super(name, requireIngredients);
+    }
+
+    // 달걀 요리에만 해당되는 공통 기능
+    void crackEgg() {
+        System.out.println("계란을 톡! 깨뜨립니다.");
+    }
+}
+
+// 빵/밀가루 요리 카테고리
+class BreadRecipe extends Recipe {
+    BreadRecipe(String name, Set<String> requireIngredients) {
+        super(name, requireIngredients);
+    }
+
+    void preheatOven() {
+        System.out.println("오븐을 예열합니다.");
+    }
+}
+
+class FriedRiceRecipe extends Recipe {
+    FriedRiceRecipe(String name, Set<String> requireIngredients) {
+        super(name, requireIngredients);
+    }
+
+    void stirFry() {
+        System.out.println("프라이팬에서 재료를 볶습니다...");
+    }
+}
+
+
+//구체화 레시피
 //레시피 작성
-class EggFry extends Recipe {
+class EggFry extends EggRecipe {
     EggFry() {
         super("계란후라이", Set.of("계란", "후라이팬"));
     }
+
+    public void cook(){
+        crackEgg();
+        super.cook();
+    }
 }
 
-class EggSteam extends Recipe {
+class EggSteam extends EggRecipe {
     EggSteam() {
         super("계란찜", Set.of("계란", "파", "소금", "냄비"));
     }
+
+    public void cook(){
+        crackEgg();
+        super.cook();
+    }
 }
 
-class EggSausageRoll extends Recipe {
+class EggSausageRoll extends EggRecipe {
     EggSausageRoll() {
         super("소시지 달걀말이", Set.of("계란", "소시지", "후라이팬"));
     }
+
+    public void cook(){
+        crackEgg();
+        super.cook();
+    }
 }
 
-class PizzaBread extends Recipe {
+class PizzaBread extends BreadRecipe {
     PizzaBread() {
         super("피자빵", Set.of("치즈", "밀가루", "오븐"));
     }
+
+    public void cook(){
+        preheatOven();
+        super.cook();
+    }
 }
 
-class Souffle extends Recipe {
+class Souffle extends BreadRecipe {
     Souffle() {
         super("수플레", Set.of("우유", "설탕", "오븐"));
     }
-}
 
-class KimchiFriedRice extends Recipe {
-    KimchiFriedRice() {
-        super("김치볶음밥", Set.of("김치", "밥", "후라이팬"));
+    public void cook(){
+        preheatOven();
+        super.cook();
     }
 }
 
-class Cake extends Recipe {
+class KimchiFriedRice extends FriedRiceRecipe {
+    KimchiFriedRice() {
+        super("김치볶음밥", Set.of("김치", "밥", "후라이팬"));
+    }
+
+    public void cook(){
+        stirFry();
+        super.cook();
+    }
+}
+
+class Cake extends BreadRecipe {
     Cake() {
         super("케이크", Set.of("계란", "밀가루", "우유", "오븐"));
+    }
+
+    public void cook(){
+        preheatOven();
+        super.cook();
     }
 }
