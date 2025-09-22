4b1a7572b27bdae117f236028c13acab393f1645
chaeyoung <happy7yong83@gmail.com>
2025-09-23 00:08:24 +0900

    refact : 밀가루 관련 레시피

--- week1/src/main/java/com/ran/BreadRecipe.java
+++ week1/src/main/java/com/ran/BreadRecipe.java
@@ -4,45 +4,39 @@ import java.util.Set;
 
 // 빵/밀가루 요리 카테고리
 public class BreadRecipe extends Recipe {
-    BreadRecipe(String name, Set<String> requireIngredients) {
+    BreadRecipe(String name, Set<Ingredient> requireIngredients) {
         super(name, requireIngredients);
     }
 
-    void preheatOven() {
-        System.out.println("오븐을 예열합니다.");
-    }
 }
 
 
 class PizzaBread extends BreadRecipe {
     PizzaBread() {
-        super("피자빵", Set.of("치즈", "밀가루", "오븐"));
+        super("피자빵", Set.of(IngredientComponents.CHEESE,IngredientComponents.OVEN,IngredientComponents.FLOURPOWDER));
     }
 
     public void cook(){
-        preheatOven();
         super.cook();
     }
 }
 
 class Souffle extends BreadRecipe {
     Souffle() {
-        super("수플레", Set.of("우유", "설탕", "오븐"));
+        super("수플레", Set.of(IngredientComponents.MILK, IngredientComponents.OVEN,IngredientComponents.EGG));
     }
 
     public void cook(){
-        preheatOven();
         super.cook();
     }
 }
 
 class Cake extends BreadRecipe {
     Cake() {
-        super("케이크", Set.of("계란", "밀가루", "우유", "오븐"));
+        super("케이크", Set.of(IngredientComponents.MILK, IngredientComponents.OVEN, IngredientComponents.FLOURPOWDER, IngredientComponents.EGG));
     }
 
     public void cook(){
-        preheatOven();
         super.cook();
     }
 }
